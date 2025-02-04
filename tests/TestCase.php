d5841578ae1c28c3498b5129b1b82232a4cd7c94
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2025-02-04 10:14:28 -0300

    Configurando ajustes finais do inicio do projeto

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -28,13 +28,3 @@ abstract class TestCase extends BaseTestCase
 {
     //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas
 }
-<?php
-
-namespace Tests;
-
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-
-abstract class TestCase extends BaseTestCase
-{
-    //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas
-}
