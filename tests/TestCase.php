2131e733c8291fde3bc9caece3dae5cfbc4242fa
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2025-02-04 10:06:34 -0300

    ok

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,3 +8,33 @@ abstract class TestCase extends BaseTestCase
 {
     //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas
 }
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas
+}
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas
+}
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas
+}
