318533a3b2e11c8c9d4639f0c4c7fb6446e7d81c
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2025-02-05 10:35:23 -0300

    reconfigurando projeto

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,24 +6,5 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas
-}
-<?php
-
-namespace Tests;
-
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-
-abstract class TestCase extends BaseTestCase
-{
-    //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas //adadadadas
-}
-<?php
-
-namespace Tests;
-
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-
-abstract class TestCase extends BaseTestCase
-{
+    //
 }
